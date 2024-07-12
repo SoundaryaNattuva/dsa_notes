d4ca4d7d4dc113a265cc3cea5d0d80ba6752ccc4
Soundarya (Arya) Nattuva <122417818+SoundaryaNattuva@users.noreply.github.com>
2024-07-12 16:47:37 -0400

    solve selection sort problem

--- sorting/12_bubble_sort.js
+++ sorting/12_bubble_sort.js
@@ -12,3 +12,21 @@ function bubbleSort(arr){
   }
   return arr
 }
+
+//optimized bubbleSort
+function bubbleSort(arr){
+  var noSwaps;
+  for (let i=arr.length; i>=0; i--){
+    noSwaps = true;
+    for (let j=0; j<i-1; j++){
+      if (arr[j] > arr[j+1]){
+        let temp = arr[j]
+        arr[j] = arr[j+1]
+        arr[j+1] = temp
+        noSwaps = false;
+      }
+    }
+    if (noSwaps) break;
+  }
+  return arr
+}
