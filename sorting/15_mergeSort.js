2db2d61c9cd7c55063ba1efb1825382c7b4d0edc
Soundarya (Arya) Nattuva <122417818+SoundaryaNattuva@users.noreply.github.com>
2024-07-17 17:00:39 -0400

    solve sort portion of merge sort

--- sorting/15_mergeSort.js
+++ sorting/15_mergeSort.js
@@ -1,8 +1,6 @@
 // Merge Sort: splits the array into halves, recursively sorts each half, and then merges the sorted halves back together.
 
-// Merging Arrays
-merge([1,10,50], [2,14,99,100])
-
+// Part #1: Merging Arrays
 function merge(arr1, arr2){
   let i=0;
   let j=0;
@@ -27,4 +25,18 @@ function merge(arr1, arr2){
   return results;
 }
 
+merge([1,10,50], [2,14,99,100])
+
 // An if statement would only check and add a single element, but a while loop processes all remaining elements until the end of the array is reached.
+
+// Part #2: sorting arrays 
+
+function mergeSort(arr){
+  if(arr.length <= 1) return arr;
+  let mid = Math.floor(arr.length/2)
+  let left = mergeSort(arr.slice(0, mid))
+  let right = mergeSort(arr.slice(mid))
+  return merge(left, right);
+}
+
+mergeSort([4, 11 ,39, 23, 7, 1, 15])
