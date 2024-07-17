c5b8979836d85832f544697f09a0f5ced11aaa99
Soundarya (Arya) Nattuva <122417818+SoundaryaNattuva@users.noreply.github.com>
2024-07-17 16:22:25 -0400

    correct merge function problem

--- sorting/15_mergeSort.js
+++ sorting/15_mergeSort.js
@@ -4,22 +4,27 @@
 merge([1,10,50], [2,14,99,100])
 
 function merge(arr1, arr2){
-  i=0;
-  j=0;
-  result=[];
-  while (i<arr1.length && j<arr2.length){
-    if (arr1[i] < arr[j]){
-      result.push(arr1[i])
+  let i=0;
+  let j=0;
+  let results =[];
+  while (i < arr1.length && j < arr2.length){
+    if (arr1[i] < arr2[j]){
+      results.push(arr1[i])
       i++;
     } else {
-      result.push(arr2[j])
+      results.push(arr2[j])
       j++;
     }
   }
-  if (i >= arr1.length){
-    result.push(arr2)
-  } else {
-    result.push(arr1)
+  while (i < arr1.length){
+    results.push(arr1[i])
+    i++;
+  } 
+  while (j < arr2.length){
+    results.push(arr2[j])
+    j++;
   }
-  return results
+  return results;
 }
+
+// An if statement would only check and add a single element, but a while loop processes all remaining elements until the end of the array is reached.
