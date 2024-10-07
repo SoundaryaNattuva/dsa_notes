887856181fcd3c63175d4916816528aa76902922
Soundarya (Arya) Nattuva <122417818+SoundaryaNattuva@users.noreply.github.com>
2024-10-07 16:11:11 -0400

    solve insert node method for BST

--- data structures/5_binary_search_trees.js
+++ data structures/5_binary_search_trees.js
@@ -19,7 +19,43 @@ class BinarySearchTree {
   constructor(){
     this.root = null;
   }
+  
+  insertNode(value){
+    let newNode = new Node(value)
+    
+    if (this.root === null){
+      this.root = newNode
+      return this;
+    } else {
+      let current = this.root;
+      while(true){
+        if(value === current.value) return undefined;
+        if(value < current.value){
+          if(current.left === null){
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left
+          }
+        } else if (value > current.value){
+          if(current.right === null){
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right
+          }
+        }
+      }
+    }  
+  }
+
+  findNode(value){
+    
+  }
 }
+
+
+
 
 let tree = new BinarySearchTree();
 tree.root = new Node(10);
